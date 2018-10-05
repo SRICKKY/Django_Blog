202047ad762fba54651ae0d066e87e0b049f6ce9
Srikant <kumar_srikant@outlook.com>
2018-10-05 21:20:06 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog',
         'USER': 'blog',
-        'PASSWORD': 'dell',
+        'PASSWORD': '##############',
     }
 }
 
